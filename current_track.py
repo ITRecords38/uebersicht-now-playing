8425076c7aadd2b75cdd07943d95b3f97a450fab
Rickard Natt och Dag <rickard@hey.com>
2020-12-22 09:35:07 +0100

    fix(sonos): handle wrong ip and display nothing while paused

--- current_track.py
+++ current_track.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from soco import SoCo
 
-ip = '192.168.1.57'
+ip = '192.168.1.55'
 
 def time(input):
     split_time = input.split(":")
@@ -12,27 +12,27 @@ def time(input):
 
 
 if __name__ == '__main__':
+    nothing = "not_playing::::::::::::::"
     sonos = SoCo(ip) # Pass in the IP of your Sonos speaker
 
     track = sonos.get_current_track_info()
     state = sonos.get_current_transport_info()
+    play_state = state['current_transport_state']
 
-    artist = track['artist']
-    name = track['title']
-    album = track['album']
-    position = track['position'][2:]
-    duration = track['duration'][2:]
-    album_art = track['album_art']
-
-    position = str(time(position))
-    duration = str(time(duration))
-
-    nothing = "not_playing::::::::::::::"
-    np = "playing::" + artist + "::" + name + "::" + album + "::::" + album_art + "::" + position + "::" + duration
-
-    if state['current_transport_state'] == "STOPPED":
+    if track['position'] == "NOT_IMPLEMENTED" or play_state == "STOPPED" or play_state == "PAUSED_PLAYBACK":
         print(nothing)
     else:
-        print(np)
+        artist = track['artist']
+        name = track['title']
+        album = track['album']
+        position = track['position'][2:]
+        duration = track['duration'][2:]
+        album_art = track['album_art']
+
+        position = str(time(position))
+        duration = str(time(duration))
+
+        print("playing::" + artist + "::" + name + "::" + album + "::::" + album_art + "::" + position + "::" + duration)
+
 
 
